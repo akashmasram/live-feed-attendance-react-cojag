f855f711ff246ebb34d1331f6f34b1b50fb95031
akashmasram <akashmasramakt@gmail.com>
2024-11-13 14:59:34 +0530

    Update server.js

--- cojag/server.js
+++ cojag/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 const path = require('path');
 const fs = require('fs');
 const cors = require('cors');
